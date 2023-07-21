af28e01abb203b345584aa44b6577aee72987e8f
annt0903 <anthanks@gmail.com>
2023-07-21 22:49:36 +0700

    update solution 10

--- src/StringPermutation.java
+++ src/StringPermutation.java
@@ -1,7 +1,7 @@
 package src;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class StringPermutation {
     private String input;
@@ -19,8 +19,8 @@ public class StringPermutation {
     }
 
     // abc -> abc, acb, bac, bca, cab, cba
-    private List<String> generatePermutation(String text){
-        var results = new ArrayList<String>();
+    private Set<String> generatePermutation(String text){
+        var results = new HashSet<String>();
         if(text.length() == 0) {
             return results;//empty
         }
@@ -30,8 +30,10 @@ public class StringPermutation {
         }
         if(text.length() == 2) {
             results.add(text);
-            results.add(String.valueOf(text.charAt(1)) + 
-                String.valueOf(text.charAt(0)));
+            String text2 = String.valueOf(text.charAt(1)) + 
+                String.valueOf(text.charAt(0));
+            //ignore dupplicate
+            if(!text.equals(text2)) results.add(text2);
             return results;//2 permutations
         }
         //text lenght >= 3 characters
@@ -52,8 +54,8 @@ public class StringPermutation {
             }
             //System.out.println("Substring of " + text + " is " + subText);
             var subPermutions = generatePermutation(subText);
-            for (String subPer : subPermutions) {
-                results.add(curChar + subPer);
+            for (String subPerm : subPermutions) {
+                results.add(curChar + subPerm);
             }
         }
         return results;
--- test/StringPermutationTest.java
+++ test/StringPermutationTest.java
@@ -11,13 +11,25 @@ public class StringPermutationTest {
     public void testSimplePermutation() {
         var permutation = new StringPermutation("a1#");
         assertEquals("Simple permutation", 6, permutation.total());
+    }
 
+    @Test
+    public void testPermutationWith4Chars() {
+        var permutation = new StringPermutation("abcd");
+        assertEquals("Simple permutation", 24, permutation.total());
     }
 
     @Test
     public void testPermutationDupplicateChars() {
         var permutation = new StringPermutation("aac");
         assertEquals("Simple permutation", 3, permutation.total());
+
+    }
+
+    @Test
+    public void testPermutationDupplicate2Chars() {
+        var permutation = new StringPermutation("cc");
+        assertEquals("Simple permutation", 1, permutation.total());
 
     }
 
