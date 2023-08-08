568ffa7278d4f2ce68b164bb43aff509b2563914
annt0903 <anthanks@gmail.com>
2023-08-08 07:17:37 +0700

    solution 104

--- test/MinMaxArray.java
+++ test/MinMaxArray.java
@@ -2,7 +2,9 @@ package test;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class MinMaxArray {
         
@@ -16,14 +18,26 @@ public class MinMaxArray {
         var minOpt = IntStream.of(numArr).min();
         if (minOpt.isPresent()) {
             return minOpt.getAsInt();
-        } else {
-            return null;
         }
-    }
-
-    public Object max() {
-        //TODO find max by filter
         return null;
     }
+
+    public Integer max() {
+        var maxOpt = IntStream.of(numArr).max();
+        if (maxOpt.isPresent()) {
+            return maxOpt.getAsInt();
+        }
+        return null;
+    }
+
+    public double avg() {
+        var avgOpt = IntStream.of(numArr).average();
+        if (avgOpt.isPresent()) {
+            return avgOpt.getAsDouble();
+        }
+        return 0.0;
+    }
+
+    //TODO: using comparator
 
 }
